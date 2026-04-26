be26018a963a2669cfa27f278c475567ae719e25
Wally <guillermo.salgado@edu.uah.es>
2026-04-26 19:53:08 +0200

    h

--- src/ParteB/Arco.java
+++ src/ParteB/Arco.java
@@ -1,7 +1,58 @@
 package ParteB;
 
-public class Arco {
+public class Arco<T> {
+    private long id;
+    private Nodo<T> origen;
+    private Nodo<T> destino;
+    private T dato;
+
+    public Arco(){
+        this.id=0;
+        this.destino=null;
+        this.origen=null;
+        this.dato=null;
+
+    }
+
+    public Arco( long id, Nodo<T> destino, Nodo<T> origen, T dato){
+        this.id=id;
+        this.destino=destino;
+        this.origen=origen;
+        this.dato=dato;
+    }
 
 
 
+    public T getDato() {
+        return dato;
+    }
+
+    public void setDato(T dato) {
+        this.dato = dato;
+    }
+
+
+    public Nodo<T> getDestino() {
+        return destino;
+    }
+
+    public void setDestino(Nodo<T> destino) {
+        this.destino = destino;
+    }
+
+    public Nodo<T> getOrigen() {
+        return origen;
+    }
+
+    public void setOrigen(Nodo<T> origen) {
+        this.origen = origen;
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
 }
